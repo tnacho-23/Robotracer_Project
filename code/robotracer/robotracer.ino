f54d2eb3cb174b5093c7b6ec3b8ffd141a5b5fdf
Ignacio Romero Aravena <ignacio.romero.a@ug.uchile.cl>
2022-07-24 22:26:19 -0400

    main code

---
+++ code/robotracer/robotracer.ino
@@ -0,0 +1,29 @@
+//Definición de componentes y sus pines
+
+int Buzzer = 2;
+int AIN1 = 3; //A: Motor Izquierdo
+int AIN2 = 4;
+int PWMA = 5; //Velocidad A
+int PWMB =6; //Velocidad B
+int BIN1 = 7; //B: Motor Derecho
+int BIN2 = 8;
+int StandBy = 9; //HIGH para QTR y Driver motor
+int Button = 10; //Botón multifunción
+int QTR1 = A0; // Sensor Frontal Extremo Izquierdo
+int QTR2 = A1;
+int QTR3 = A2;
+int QTR4 = A3;
+int QTR5 = A4;
+int QTR6 = A5; //Sensor Frontal Extremo Derecho
+int TCR1 = A6; //Sensor lateral Izquierdo
+int TCR2 = A7; //Sensor lateral Derecho
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
