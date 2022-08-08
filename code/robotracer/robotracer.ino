9d6aee178658e9b9d2624c9126459ef6a9797ede
Ignacio Romero Aravena <ignacio.romero.a@ug.uchile.cl>
2022-08-07 21:38:34 -0400

    avances en el código

--- code/robotracer/robotracer.ino
+++ code/robotracer/robotracer.ino
@@ -18,12 +18,82 @@ int QTR6 = A5; //Sensor Frontal Extremo Derecho
 int TCR1 = A6; //Sensor lateral Izquierdo
 int TCR2 = A7; //Sensor lateral Derecho
 
+
+//Setup
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(Buzzer, OUTPUT);
+  pinMode(AIN1, OUTPUT);
+  pinMode(AIN2,OUTPUT);
+  pinMode(PWMA, OUTPUT);
+  pinMode(PWMB,OUTPUT);
+  pinMode(BIN1,OUTPUT);
+  pinMode(BIN2,OUTPUT);
+  pinMode(StandBy,OUTPUT);
+  pinMode(Button,INPUT);
+  pinMode(QTR1,INPUT);
+  pinMode(QTR2,INPUT);
+  pinMode(QTR3,INPUT);
+  pinMode(QTR4,INPUT);
+  pinMode(QTR5,INPUT);
+  pinMode(QTR6,INPUT);
+  pinMode(TCR1,INPUT);
+  pinMode(TCR2,INPUT);
+  digitalWrite(StandBy,HIGH);
+  Serial.begin(9600);
+}
+
+//Main Loop
+void loop() {
+  motor_der("atrás", 100);
+  delay(3000);
+  motor_der("atrás", 255);
+  delay(1000);
+  motor_der("atrás",100);
+  delay(1000);
+  motor_der("adelante",100);
+  delay(500);
+  motor_der("adelante",255);
+  delay(3000);
+  motor_der("apagado",0);
+  delay(2000);
+  
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+//Funciones Auxiliares
 
+void motor_izq(String movimiento, int velocidad){
+  if(movimiento == "adelante"){
+    digitalWrite(AIN1,LOW);
+    digitalWrite(AIN2,HIGH);
+    analogWrite(PWMA,velocidad);
+  }
+  if(movimiento == "atrás"){
+    digitalWrite(AIN1,HIGH);
+    digitalWrite(AIN2, LOW);
+    analogWrite(PWMA,velocidad);
+  }
+  if(movimiento == "apagado"){
+    digitalWrite(AIN1,LOW);
+    digitalWrite(AIN2, LOW);
+    analogWrite(PWMA,0);
+  }
+}
+
+void motor_der(String movimiento, int velocidad){
+  if(movimiento == "adelante"){
+    digitalWrite(BIN1,LOW);
+    digitalWrite(BIN2,HIGH);
+    analogWrite(PWMB,velocidad);
+  }
+  if(movimiento == "atrás"){
+    digitalWrite(BIN1,HIGH);
+    digitalWrite(BIN2, LOW);
+    analogWrite(PWMB,velocidad);
+  }
+  if(movimiento == "apagado"){
+    digitalWrite(BIN1,LOW);
+    digitalWrite(BIN2, LOW);
+    analogWrite(PWMB,0);
+  }
 }
